046d5c1d88e4d5f352f367f978c6a5d2adbd7263
hab278 <hab278@nyu.edu>
2014-02-19 15:48:10 -0500

    Add git_handler.rb to complete rename.

---
+++ lib/figs/git_handler.rb
@@ -0,0 +1,40 @@
+require "git"
+
+module Figs
+  module GitHandler
+    extend self
+    TMP_GIT_DIR = "tmp/figs/"
+    
+    def location gitpath, filenames
+      git_clone gitpath
+      tmp_filenames(([]<<filenames).flatten)
+    rescue
+      clear_tmp_dir
+    end
+    
+    private
+    
+    def tmp_filenames filenames
+      tmp_files = []
+      filenames.each { |filename| tmp_files << copy_to_tmp_files(filename) }
+      clear_tmp_dir
+      tmp_files
+    end
+    
+    def copy_to_tmp_files filename
+      Tempfile.open("#{filename}") do |file|
+        file.write(File.open("#{TMP_GIT_DIR}#{filename}").read)
+        file.path
+      end
+    end
+    
+    def git_clone gitpath
+      clear_tmp_dir
+      ::Git.clone gitpath, TMP_GIT_DIR
+    end
+    
+    def clear_tmp_dir
+      FileUtils.rm_rf TMP_GIT_DIR
+    end
+  end
+end
