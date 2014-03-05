dbacb534a94145c426b9eda4dd2a911e13654b2a
hab278 <hab278@nyu.edu>
2014-03-05 18:17:20 -0500

    Test for directory parsing.

--- spec/figs/directory_flattener_spec.rb
+++ spec/figs/directory_flattener_spec.rb
@@ -1,12 +1,23 @@
 require "spec_helper"
 
+SPEC_TMP_DIR = "tmp/flattener"
+
 describe Figs::DirectoryFlattener do
-  subject (:directoryflattener) { Figs::DirectoryFlattener.flattened_filenames(["spec"]) }
+  def create_tmp_dir
+    FileUtils.mkdir_p("#{SPEC_TMP_DIR}")
+    File.open("#{SPEC_TMP_DIR}/config.yml", File::WRONLY|File::CREAT|File::EXCL)
+    File.open("#{SPEC_TMP_DIR}/config2.yml", File::WRONLY|File::CREAT|File::EXCL)
+  end
+  
+  before(:each) do
+    create_tmp_dir
+  end
+  subject (:directoryflattener) { Figs::DirectoryFlattener.flattened_filenames([SPEC_TMP_DIR]) }
   
   describe "#flattened_filenames" do
     context "directories" do
       subject { directoryflattener } 
-      # it { should eq(["spec/figs/application_spec.rb", "spec/figs/directory_flattener_spec.rb", "spec/figs/env_spec.rb", "spec/figs/figfile_spec.rb", "spec/figs_spec.rb", "spec/spec_helper.rb", "spec/support/aruba.rb", "spec/support/random.rb", "spec/support/reset.rb"]) }
+      it { should eq(["tmp/flattener/config.yml", "tmp/flattener/config2.yml"]) }
     end
   end
 end
