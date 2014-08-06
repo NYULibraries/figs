a04bc15e2f0e69b6b8e5b5f0bba54debe2b9ed7a
Scot Dalton <scotdalton@gmail.com>
2014-08-06 11:51:49 -0400

    Add specs for ENV#set including failing specifications for Ruby "basic types" implicitly converted by YAML
    This commit addresses issue #13.
    
    Since Ruby's implementation of YAML has a set of ["basic types"](http://www.yaml.org/YAML_for_ruby.html#basic_types) that are implicitly converted from String to the appropriate type, Figs should convert the "basic types" to strings and store them in ENV, so that a calling application can just call ENV['VAR_NAME'] for "basic types", which is more intuitive.
    
    Basic types handled by YAML and included in the specifications for ENV#set:
    - Integer
    - Float
    - Null
    - Boolean
    - Time (in the ISO8601 format)
    - Date (in the ISO8601 format)

--- spec/figs/env_spec.rb
+++ spec/figs/env_spec.rb
@@ -18,6 +18,93 @@ describe Figs::ENV do
     ENV.delete("foo")
     env.delete("arr")
   end
+
+  describe '#set' do
+    after { ENV.delete(key) }
+    let(:key) { 'VAR_NAME' }
+    before { env.set(key, value) }
+    context 'when the value is a String' do
+      let(:value) { 'value' }
+      it 'should set the String in the environment' do
+        expect(ENV[key]).to eq 'value'
+      end
+      it 'should equal the value in the Figs::ENV' do
+        expect(env[key]).to eq value
+      end
+    end
+    context 'when the value is an Integer' do
+      let(:value) { 1 }
+      it 'should set the Integer as a String in the environment' do
+        expect(ENV[key]).to eq '1'
+      end
+      it 'should equal the value in the Figs::ENV' do
+        expect(env[key]).to eq value
+      end
+    end
+    context 'when the value is a Float' do
+      let(:value) { 2.0 }
+      it 'should set the Float as a String in the environment' do
+        expect(ENV[key]).to eq '2.0'
+      end
+      it 'should equal the value in the Figs::ENV' do
+        expect(env[key]).to eq value
+      end
+    end
+    context 'when the value is true' do
+      let(:value) { true }
+      it 'should set true as a String in the environment' do
+        expect(ENV[key]).to eq 'true'
+      end
+      it 'should be true in the Figs::ENV' do
+        expect(env[key]).to be true
+      end
+    end
+    context 'when the value is false' do
+      let(:value) { false }
+      it 'should set false as a String in the environment' do
+        expect(ENV[key]).to eq 'false'
+      end
+      it 'should be false in the Figs::ENV' do
+        expect(env[key]).to be false
+      end
+    end
+    context 'when the value is a Time' do
+      let(:value) { Time.now }
+      it 'should set the Time as a String in ISO8601 format in the environment' do
+        expect(ENV[key]).to eq value.iso8601
+      end
+      it 'should be within a second of the value in the Figs::ENV' do
+        expect(env[key]).to be_within(1).of value
+      end
+    end
+    context 'when the value is a Date' do
+      let(:value) { Time.now.to_date }
+      it 'should set the Date in ISO8601 format in the environment' do
+        expect(ENV[key]).to eq value.iso8601
+      end
+      it 'should equal the value in the Figs::ENV' do
+        expect(env[key]).to eq value
+      end
+    end
+    context 'when the value is nil' do
+      let(:value) { nil }
+      it 'should set the value "~" in the environment' do
+        expect(ENV[key]).to eq '~'
+      end
+      it 'should be nil in the Figs::ENV' do
+        expect(env[key]).to be_nil
+      end
+    end
+    context 'when the value is not a YAML for Ruby "basic type"' do
+      let(:value) { {key: 1} }
+      it 'should set a YAML string representation of the value in the environment' do
+        expect(ENV[key]).to eq "---\n:key: 1\n"
+      end
+      it 'should equal the value in the Figs::ENV' do
+        expect(env[key]).to eq value
+      end
+    end
+  end
   
   describe "#[]=" do
     context "objects" do
