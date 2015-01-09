033e260eac2d4e0fb8c901b55f8bda1b43281684
hab278 <hab278@nyu.edu>
2015-01-09 16:44:00 -0500

    Fix #17 by cleaning code

--- lib/figs/git_handler.rb
+++ lib/figs/git_handler.rb
@@ -11,25 +11,28 @@ module Figs
       git_clone gitpath
       temp_filenames(([]<< Figs::DirectoryFlattener.flattened_filenames(filenames.collect {|filename| "#{TMP_GIT_DIR}#{filename}"})).flatten)
     rescue Exception => e
-      p e
+      puts e
       clear_tmp_dir
       clear_temp_files
     end
-    
+
     def clear_temp_files
       return unless !@temp_files.nil?
-      @temp_files.each { |temp_file| temp_file.close temp_file.unlink }
+      @temp_files.each do |temp_file|
+        temp_file.close
+        temp_file.unlink
+      end
     end
-    
+
     private
-    
+
     def temp_filenames(filenames)
       temp_files = []
       filenames.each { |filename| temp_files << copy_to_temp_files(filename) }
       clear_tmp_dir
       temp_files
     end
-    
+
     def copy_to_temp_files(filename)
       temp_file = Tempfile.new("#{filename.gsub('/','-')}")
       temp_file.open
@@ -38,12 +41,12 @@ module Figs
       @temp_files << temp_file
       temp_file.path
     end
-    
+
     def git_clone(gitpath)
       clear_tmp_dir
       ::Git.clone gitpath, TMP_GIT_DIR
     end
-    
+
     def clear_tmp_dir
       FileUtils.rm_rf TMP_GIT_DIR
     end
